20dccbd04dc57affc3f2e795e4ef3da2c61c20a9
Doric77 <paza55@op.pl>
2025-03-31 11:27:47 +0200

    Add files via upload

---
+++ zadaniaaaa/src/LibraryManagementSystem/Book.java
@@ -0,0 +1,40 @@
+package LibraryManagementSystem;
+public class Book {
+    // Pola: tytuł, autor, ISBN
+    private String title;
+    private String author;
+    private String isbn;
+    // Konstruktor z walidacją danych (rzuca wyjątek)
+    public Book(String title, String author, String isbn) {
+    this.title = title;
+    this.author =author;
+    this.isbn =isbn;
+
+
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
---
+++ zadaniaaaa/src/LibraryManagementSystem/ConsoleBookInput.java
@@ -0,0 +1,10 @@
+package LibraryManagementSystem;
+import java.util.Scanner;
+public class ConsoleBookInput implements BookInput {
+    private Scanner scanner = new Scanner(System.in);
+    @Override
+    public Book readBook() throws InvalidBookException {
+        // Wczytywanie danych z konsoli i tworzenie obiektu Book
+        return null;
+    }
+}
